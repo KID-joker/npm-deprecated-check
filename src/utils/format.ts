deb46695ce9b01021fe0a911dae1175b89012b39
KID-joker <kid_joker@126.com>
2024-10-21 20:47:27 +0800

    fix: optimize regex matching

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,15 +1,11 @@
-/* eslint-disable regexp/no-super-linear-backtracking */
 import type { VersionOrRange } from '../types'
 
 export function formatDependencies(dependencies: Record<string, string>): Record<string, VersionOrRange> {
   const newDependencies: Record<string, VersionOrRange> = {}
-  for (let packageName in dependencies) {
-    if (/^npm:.+@.+$/.test(dependencies[packageName])) {
-      const result = /^npm:.+@.+$/.exec(dependencies[packageName])
-      if (result) {
-        packageName = result[1]
-        dependencies[packageName] = result[2]
-      }
+  for (const packageName in dependencies) {
+    if (dependencies[packageName].includes('@')) {
+      const idx = dependencies[packageName].lastIndexOf('@')
+      dependencies[packageName] = dependencies[packageName].slice(idx + 1)
     }
     newDependencies[packageName] = {
       range: dependencies[packageName],
