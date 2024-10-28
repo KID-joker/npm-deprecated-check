e4a7f403a9d41978ee5817891a90e0b81e4e7432
KID-joker <kid_joker@126.com>
2024-10-28 23:10:28 +0800

    fix: remove unused

--- src/io/node.ts
+++ src/io/node.ts
@@ -7,40 +7,34 @@ import { coerce, gt, major } from 'semver'
 function getLatestNodeVersion() {
   const versions = Object.keys(nodeReleases)
   const latestVersion = versions.reduce((_prev, _curr) => {
-    const prev = coerce(_prev)
-    const curr = coerce(_curr)
+    const prev = coerce(_prev)!
+    const curr = coerce(_curr)!
     return gt(curr, prev) ? _curr : _prev
   })
   return latestVersion
 }
 
 function checkNode() {
-  const nodeVersion = coerce(process.version)
-  const latestNodeVersion = coerce(getLatestNodeVersion())
+  const nodeVersion = coerce(process.version)!
+  const latestNodeVersion = coerce(getLatestNodeVersion())!
   const nodeVersionData = nodeReleases[`v${major(nodeVersion)}` as keyof typeof nodeReleases]
-  let result = false
   if (nodeVersionData) {
     const endDate = new Date(nodeVersionData.end)
     const currentDate = new Date()
     const isNodeVersionSupported = currentDate < endDate
     if (isNodeVersionSupported) {
       console.log(chalk.green(`Your node version (${nodeVersion}) is supported until ${nodeVersionData.end}.`))
-      result = true
     }
     else {
       console.log(chalk.yellow(`Your node version (${nodeVersion}) is no longer supported since ${nodeVersionData.end}.`))
-      result = false
     }
   }
   else if (gt(nodeVersion, latestNodeVersion)) {
     console.log(chalk.yellow(`Your node version (${nodeVersion}) is higher than the latest version ${latestNodeVersion}. Please update 'npm-deprecated-check'.`))
-    result = true
   }
   else {
     console.log(chalk.yellow(`Your node version (${nodeVersion}) can't be found in the release schedule. Please update 'npm-deprecated-check'.`))
-    result = false
   }
-  return result
 }
 
 export default checkNode
