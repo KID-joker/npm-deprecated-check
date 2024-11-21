261f843c027abb6f8399aceb1eb7451373b6f71c
KID-joker <kid_joker@126.com>
2024-11-21 15:40:07 +0800

    test: install request force

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ test('current tests', async (t) => {
   })
 
   await t.test('check if deprecation warning is shown if deprecated package is installed', async (_t) => {
-    await exec('pnpm i request')
+    await exec('pnpm i request --force')
     const { stderr } = await exec('node ./dist/cli.mjs current', { timeout: 160000 })
     assert.ok(/request has been deprecated/.test(stderr), 'Expected "has been deprecated" to be mentioned in deprecation warning.')
     // Cleanup: Undo the installation
