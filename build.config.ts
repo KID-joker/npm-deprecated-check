b2e18ae7e6ab05da135837d296e7b501cc3b6223
KID-joker <kid_joker@126.com>
2025-05-16 00:28:25 +0800

    release 1.3.1

--- build.config.ts
+++ build.config.ts
@@ -2,8 +2,12 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['src/cli'],
+  declaration: true,
   rollup: {
     inlineDependencies: true,
+    esbuild: {
+      minify: true,
+    },
   },
   clean: true,
 })
