9c2a3a80ab2cecb1bcc9b8ca540d82666d32e90e
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-07-22 20:21:49 +0200

    test: update pnpm install command and improve deprecation warning assertions

--- test/global.spec.js
+++ test/global.spec.js
@@ -12,7 +12,7 @@ const managers = ['npm', 'yarn', 'pnpm']
 const installCommands = {
   npm: 'npm i -g --force',
   yarn: 'yarn global add --force',
-  pnpm: 'pnpm add -g --force',
+  pnpm: 'pnpm i -g --force',
 }
 const uninstallCommands = {
   npm: 'npm un -g',
@@ -20,13 +20,16 @@ const uninstallCommands = {
   pnpm: 'pnpm remove -g',
 }
 
+// eslint-disable-next-line no-control-regex
+const deprecatedRegex = /^\u001B\[33mdeprecated:/gm
+
 async function check(manager, t) {
   try {
     execSync(`${installCommands[manager]} eslint`)
 
     await t.test(`check ${manager} that no deprecation warning is shown`, (_t, done) => {
       exec(`node ${cli} global --manager ${manager}`, (_error, _stdout, stderr) => {
-        assert.ok(!/deprecated/.test(stderr), 'Not expected "deprecated" to be mentioned in deprecation warning.')
+        assert.doesNotMatch(stderr, deprecatedRegex, 'Not expected "deprecated" to be mentioned in deprecation warning.')
         done()
       })
     })
@@ -35,39 +38,42 @@ async function check(manager, t) {
 
     await t.test(`check ${manager} that deprecation warning is shown if deprecated package is installed`, (_t, done) => {
       exec(`node ${cli} global --manager ${manager}`, { timeout: 160000 }, (_error, _stdout, stderr) => {
-        assert.ok(/deprecated/.test(stderr), 'Expected "deprecated" to be mentioned in deprecation warning.')
+        assert.match(stderr, deprecatedRegex, 'Expected "deprecated" to be mentioned in deprecation warning.')
         done()
       })
     })
 
     await t.test(`check ${manager} that no deprecation warning is shown if ignore deprecated package`, (_t, done) => {
-      exec(`node ${cli} global --manager ${manager} --ignore tslint,vue-cli`, { timeout: 160000 }, (_error, _stdout, stderr) => {
-        assert.ok(!/deprecated/.test(stderr), 'Not expected "deprecated" to be mentioned in deprecation warning.')
+      exec(`node ${cli} global --manager ${manager} --ignore request,tslint,vue-cli`, { timeout: 160000 }, (_error, _stdout, stderr) => {
+        assert.doesNotMatch(stderr, deprecatedRegex, 'Not expected "deprecated" to be mentioned in deprecation warning.')
         done()
       })
     })
 
     await t.test(`check ${manager} that exit the program if the package is deprecated`, (_t, done) => {
       exec(`node ${cli} global --manager ${manager} --failfast`, { timeout: 160000 }, (error, _stdout, stderr) => {
-        // eslint-disable-next-line no-control-regex
-        assert.strictEqual((stderr.match(/^\u001B\[33mdeprecated:/gm) || []).length, 1, 'Expected exactly one deprecation warning')
+        const deprecatedMatches = (stderr.match(deprecatedRegex) || []).length
+        assert.strictEqual(deprecatedMatches, 1, 'Expected exactly one deprecation warning')
         assert.strictEqual(error?.code, 1, 'Expected process to exit with code 1')
+
         done()
       })
     })
-  }
-  finally {
-    ['eslint', 'tslint', 'vue-cli'].forEach((dep) => {
-      try {
-        execSync(`${uninstallCommands[manager]} ${dep}`)
-      }
-      catch {}
+
+    // Cleanup
+    t.after(() => {
+      execSync(`${uninstallCommands[manager]} eslint tslint vue-cli`)
     })
+  }
+  catch (error) {
+    console.error(`Error during tests for ${manager}:`, error)
+    throw error
   }
 }
 
 test('global tests', async (t) => {
-  await Promise.all(managers.map(async (manager) => {
+  // Run tests
+  for (const manager of managers) {
     await check(manager, t)
-  }))
+  }
 })
