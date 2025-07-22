1bfd6c74092cc59834126e0bbcf90fb789e87ee0
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-07-22 20:21:49 +0200

    test: improve deprecation warning assertions and refactor test setup

--- test/global.spec.js
+++ test/global.spec.js
@@ -12,7 +12,7 @@ const managers = ['npm', 'yarn', 'pnpm']
 const installCommands = {
   npm: 'npm i -g --force',
   yarn: 'yarn global add --force',
-  pnpm: 'pnpm i -g --force',
+  pnpm: 'pnpm add -g --force',
 }
 const uninstallCommands = {
   npm: 'npm un -g',
