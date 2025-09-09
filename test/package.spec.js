32966b0b12e5fa496f23dde919281cdef877c619
i529015 <yaman.kumar.sahu@sap.com>
2025-09-09 19:52:42 +0530

    added test cases for minimumVersionUpgrade

--- test/package.spec.js
+++ test/package.spec.js
@@ -23,3 +23,38 @@ test('package tests', async (t) => {
     })
   })
 })
+
+test('shows minimum upgrade version for deprecated package', async (t) => {
+  await t.test('should display minimum upgrade version', (_t, done) => {
+    exec(`node ${cli} package tslint`, (_error, _stdout, stderr) => {
+      const output = _stdout + stderr;
+      const match = output.match(/minimum upgrade version[^\[]*\[([^\]]+)\]/);
+      // the latest non-deprecated version of tslint is 6.0.0-beta1 which is a beta version
+      // 39m\n[\x1B[35m is part of the ANSI color codes added by the CLI output
+      // so we include it in the expected version to match exactly what is output
+      const expectedVersion = '39m\n[\x1B[35m6.0.0-beta1\x1B[39m';
+      const actualVersion = match ? match[1] : undefined;
+      assert.strictEqual(actualVersion, expectedVersion, `Expected version: ${expectedVersion}, Actual version: ${actualVersion}`);
+      assert.ok(/minimum upgrade version/.test(output), 'Expected minimum upgrade version to be mentioned.')
+      done()
+    })
+  })
+})
+
+test('shows all-deprecated message when no upgrade available', async (t) => {
+  await t.test('should display all-deprecated message', (_t, done) => {
+    exec(`node ${cli} package vue-cli`, (_error, _stdout, stderr) => {
+      assert.ok(/All available versions are deprecated/.test(_stdout), 'Expected all-deprecated message to be mentioned.')
+      done()
+    })
+  })
+})
+
+test('does not show all-deprecated message when upgrade is available', async (t) => {
+  await t.test('should not display all-deprecated message', (_t, done) => {
+    exec(`node ${cli} package tslint`, (_error, _stdout, stderr) => {
+      assert.ok(!/All available versions are deprecated/.test(_stdout + stderr), 'Not expected all-deprecated message to be mentioned.')
+      done()
+    })
+  })
+})
